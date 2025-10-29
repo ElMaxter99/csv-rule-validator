be6e7d035929abc3f9f6795f2ceaf4c9681ed3d8
Alvaro Maxter <themaxter99@gmail.com>
2025-10-29 10:21:13 +0100

    Add initial .gitignore, parseCSV utility, and validators index

---
+++ validators/index.js
@@ -0,0 +1,13 @@
+import required from './required.js';
+import number from './number.js';
+import inRule from './in.js';
+import requiredIf from './requiredIf.js';
+import custom from './custom.js';
+
+export default {
+  required,
+  number,
+  in: inRule,
+  requiredIf,
+  custom,
+};
